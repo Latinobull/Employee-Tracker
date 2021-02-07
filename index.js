8c748200713d3ef91570cb2a07665fafe7437dba
Donnahue George <Donnahuegjr@gmail.com>
2021-02-06 21:10:29 -0500

    finished start function

--- index.js
+++ index.js
@@ -13,4 +13,80 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  start();
 });
+
+function start() {
+  console.log("\n");
+  inquirer
+    .prompt({
+      type: "list",
+      name: "menu",
+      message: "Welcome to the Employee Tracker Application\n",
+      pageSize: 25,
+      choices: [
+        new inquirer.Separator("-----Employees-----"),
+        "view employees",
+        "edit employees",
+        "add employees",
+        "delete employees",
+        new inquirer.Separator("\n-------Roles-------"),
+        "view roles",
+        "edit roles",
+        "add roles",
+        "delete roles",
+        new inquirer.Separator("\n----Departments----"),
+        "view departments",
+        "edit departments",
+        "add departments",
+        "delete departments",
+        new inquirer.Separator("\n-------Budget-------"),
+        "view budget",
+      ],
+    })
+    .then(function (ans) {
+      switch (ans.menu) {
+        case "view employees":
+          emp.view();
+          break;
+        case "edit employees":
+          emp.edit();
+          break;
+        case "add employees":
+          emp.add();
+          break;
+        case "delete employees":
+          emp.delete();
+          break;
+        case "view roles":
+          viewDB("role");
+          break;
+        case "edit roles":
+          role.edit();
+          break;
+        case "add roles":
+          role.add();
+          break;
+        case "delete roles":
+          role.delete();
+          break;
+        case "view departments":
+          viewDB("department");
+          break;
+        case "edit departments":
+          dep.edit();
+          break;
+        case "add departments":
+          dep.add();
+          break;
+        case "delete departments":
+          dep.delete();
+          break;
+        case "view budget":
+          budget();
+          break;
+        default:
+          break;
+      }
+    });
+}

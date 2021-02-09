4f0a6f91a70c7b450fa4010ba6395b4465cf9242
Donnahue George <Donnahuegjr@gmail.com>
2021-02-08 19:29:26 -0500

    added employees to the database

--- db/seed.sql
+++ db/seed.sql
@@ -19,3 +19,16 @@ INSERT INTO role (department_id, title, salary) VALUES
 (2, "Accounts Manager", 120000),
 (3, "Chief HR Officer", 195000),
 (4, "Engineering Manager", 150000);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+-- regular
+("Jeremiah", "Vega", 1, 1)
+("Johnny", "Perez", 2,2)
+("Amelia", "Lopez", 3, 3)
+("Shelly", "Landrau", 4,4)
+-- managers
+("Donnahue", "George", 11, NULL)
+("Caroline", "Lynch", 12, NULL)
+("Fiach", "Hill", 13, NULL)
+("Alex", "Wooley", 14, NULL)
